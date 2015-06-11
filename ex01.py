5f45430adfffd6ee415c87c79458e7da37f7c44e
Hank <hanksohota@users.noreply.github.com>
2015-06-11 10:14:37 +0100

    Create ex01.py
    This is the first exercise in the book and it simply covers use of the 'print' statement.  The last couple of lines in the code introduce the issue of using both single and double quotes.

---
+++ ex01.py
@@ -0,0 +1,9 @@
+# Simple 'print' statement
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay!  Printing.'
+print "I'd much rather you 'not'."
+# 'print' statement with both single and double quotes
+print 'I "said" do not touch this.'
